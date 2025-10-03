122d91dc52600aabc52714f083372222af908cee
Maiko Rafael Spiess <48365668+spiessbnu@users.noreply.github.com>
2025-10-03 19:28:59 -0300

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -12,6 +12,20 @@ from sklearn.decomposition import PCA
 from sklearn.cluster import KMeans
 import ast
 
+st.markdown("""
+    <style>
+        [data-testid="stSidebar"] {
+            background-color: #0F5EDD;
+            color: white;
+        }
+        /* Garante que os links de navegação fiquem brancos */
+        [data-testid="stSidebar"] a {
+            color: white;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+
 # --------------------------------------------------------------------------
 # CONFIGURAÇÃO DE ARQUIVOS E CONSTANTES
 # --------------------------------------------------------------------------
